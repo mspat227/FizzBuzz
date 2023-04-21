3a575871054e81720a83899369546cb128d717f0
Mayura Pathirana <mayurapathirana@ymail.com>
2023-04-21 21:52:23 +0530

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,16 +3,18 @@ print integers one to N, but print'Fizz' if an integer is divisible by three,
 'Buzz' if integer divisible by five, and 'FizzBuzz' if integer is divisible by both
 three and five
 '''
-
+#get the number from the user and raise error if value not a int
 try:
     n = int(input("Number: ").strip())
 
 except ValueError:
     print("Value is not an integer")
 
-
+#loop through numbers till n starting from one 
 for i in range(1, n + 1, 1):
+    #if number divide by three 
     if i % 3 == 0:
+        #and same number divide by five too
         if i % 5 == 0:
             print(f"[{i}] FizzBuzz")
         else:
